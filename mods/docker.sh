490e54922b66f1437cc995af3942f65114eb13ce
tnaky <michibata.dev@gmail.com>
2021-09-10 19:04:09 +0900

    docker コマンドの補完を追加

--- mods/docker.sh
+++ mods/docker.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/zsh
 
+if type docker &> /dev/null; then
+    local docker_ver=v$(docker version --format '{{.Client.Version}}')
+    local docker_dir=${HOME}/.local/short/docker/completion/${docker_ver}
+    local completion=${docker_dir}/_docker
+
+    if [ ! -d ${docker_dir} ]; then
+        mkdir -p ${docker_dir}
+        curl -sSL https://raw.githubusercontent.com/docker/cli/${docker_ver}/contrib/completion/zsh/_docker > ${completion}
+        ln -sf ${completion} ${XDG_CONFIG_HOME}/zsh/completions/aliases/_docker
+    fi
+
+    autoload -Uz _docker
+    unset docker_ver docker_dir completion
+fi
+
 if type docker-compose &> /dev/null; then
     local docker_compose_ver=$(docker-compose version --short)
     local docker_compose_dir=${HOME}/.local/share/docker/completion/${docker_compose_ver}
