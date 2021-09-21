c8527db38808b629945367e88cbf767d830ca2e2
tnaky <michibata.dev@gmail.com>
2021-09-21 17:28:11 +0900

    vuejsを使用するための環境を追加

---
+++ mods/node.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/zsh
+
+export N_PREFIX=${HOME}/.local/share/n
+
+if [ ! -d ${N_PREFIX} ]; then
+    curl -sSL https://git.io/n-install | bash -s -- -y -n lts
+fi
+
+[[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+
+if ! type vue &> /dev/null; then
+    npm install -g @vue/cli
+fi
