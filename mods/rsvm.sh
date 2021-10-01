e8c56529bebd6c7035da759cc9160505bfbb0837
tnaky <michibata.dev@gmail.com>
2021-10-01 20:41:10 +0900

    rsvmの設定で合わせてcargoの入力補完を設定するように修正

--- mods/rsvm.sh
+++ mods/rsvm.sh
@@ -1,21 +1,21 @@
-RSVM_TARGET=${HOME}/.local/share/rsvm
-RSVM_DIR=${HOME}/.cache/rsvm
+RSVM_DIR=${HOME}/.local/share/rsvm
 
-if [ ! -d ${RSVM_TARGET} ]; then
-    mkdir -p ${RSVM_TARGET}
-    git clone https://github.com/sdepold/rsvm.git ${RSVM_TARGET}
-fi
+test -d ${RSVM_DIR} || git clone https://github.com/sdepold/rsvm.git ${RSVM_DIR}
+test -d ${RSVM_DIR}/cache || mkdir -p ${RSVM_DIR}/cache
 
-if [ ! -d ${RSVM_DIR} ]; then
-    mkdir -p ${RSVM_DIR}/cache
-fi
+[[ -s "${RSVM_DIR}/rsvm.sh" ]] && . ${RSVM_DIR}/rsvm.sh
 
-[[ -s "${RSVM_TARGET}/rsvm.sh" ]] && . ${RSVM_TARGET}/rsvm.sh
+test -f ${RSVM_DIR}/current/dist/share/zsh/site-functions/_cargo -a ! -f ${XDG_CONFIG_HOME}/zsh/completions/aliases/_cargo && \
+    ln -s ${RSVM_DIR}/current/dist/share/zsh/site-functions/_cargo ${XDG_CONFIG_HOME}/zsh/completions/aliases/_cargo
+autoload -Uz _cargo
 
 rsvmon()
 {
     if [ -d ${RSVM_DIR}/versions ]; then
         ver=$(ls ${RSVM_DIR}/versions | fzf)
         rsvm use ${ver}
+        test -f ${RSVM_DIR}/current/dist/share/zsh/site-functions/_cargo && \
+            ln -sf ${RSVM_DIR}/current/dist/share/zsh/site-functions/_cargo ${XDG_CONFIG_HOME}/zsh/completions/aliases/_cargo
+        autoload -Uz _cargo
     fi
 }
